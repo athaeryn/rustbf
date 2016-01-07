f441d19307001a0eb63ec4eb3e9f8a70d98b08d5
Mike Anderson <hello@mrmikea.com>
2016-01-06 23:33:55 -0600

    Implement execution enough to run 'Hello, world!'
    I haven't implemented Command::InputByte yet. Also, I've got a lot of
    cleaning up to do!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+#[derive(PartialEq)]
 enum Command {
     IncrementPointer,
     DecrementPointer,
@@ -26,28 +27,11 @@ impl fmt::Display for Command {
     }
 }
 
-
 fn main() {
     // Prints 'Hello, world!'
     // From https://esolangs.org/wiki/Hello_world_program_in_esoteric_languages
-    let program = "
-        ++++++++++
-        [
-            >+++++++
-            >++++++++++
-            >+++
-            >+
-            <<<<-
-        ] end of the loop
-        >++ .
-        >+ .
-        +++++++ .. +++ .
-        >++ .
-        << +++++++++++++++ .
-        > . +++ . ------ . -------- .
-        > + .
-        >.
-    ";
+    let program = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++
+                   .>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
 
     let mut commands: Vec<Command> = Vec::new();
 
@@ -69,7 +53,69 @@ fn main() {
         }
     }
 
-    for command in commands {
-        print!("{}", command);
+    run(commands);
+}
+
+fn run(commands: Vec<Command>) {
+    let mut tape = [0; 30000];
+    let mut ptr = 0;
+
+    let mut last_forward_jump = 0;
+    let mut cmd_ptr = 0;
+
+    loop {
+        match commands[cmd_ptr] {
+            Command::IncrementPointer => {
+                if ptr < tape.len() { ptr += 1 };
+                cmd_ptr += 1;
+            },
+            Command::DecrementPointer => {
+                if ptr > 0 { ptr -= 1 };
+                cmd_ptr += 1;
+            },
+            Command::IncrementByte => {
+                tape[ptr] += 1;
+                cmd_ptr += 1;
+            },
+            Command::DecrementByte => {
+                tape[ptr] -= 1;
+                cmd_ptr += 1;
+            },
+            Command::OutputByte => {
+                let c = tape[ptr] as u8 as char;
+                print!("{}", c);
+                cmd_ptr += 1;
+            },
+            Command::InputByte => { cmd_ptr += 1 }, // TODO
+            Command::JumpForward => {
+                last_forward_jump = cmd_ptr;
+                if tape[ptr] == 0 {
+                    cmd_ptr += 1;
+                    loop {
+                        if commands[cmd_ptr] != Command::JumpBackward {
+                            break
+                        }
+                        cmd_ptr += 1;
+                        if cmd_ptr >= commands.len() {
+                            break
+                        }
+                    }
+                    cmd_ptr += 1;
+                } else {
+                    cmd_ptr += 1;
+                }
+            },
+            Command::JumpBackward => {
+                if tape[ptr] != 0 {
+                    cmd_ptr = last_forward_jump;
+                } else {
+                    cmd_ptr += 1;
+                }
+            }
+        }
+        if cmd_ptr >= commands.len() {
+            break
+        }
     }
 }
+
